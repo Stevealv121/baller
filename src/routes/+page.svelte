9234450a8ffaa0bbe452a29412800c36c5b48dd4
Steve <stevealv@hotmail.com>
2024-02-18 20:43:06 -0600

    Top scorers bare bones

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,68 @@
 <script>
-	import Test from '$lib/Test.svelte';
-	export let data;
+	let players = [
+		{
+			name: 'Cristiano',
+			goals: 10
+		},
+		{
+			name: 'Messi',
+			goals: 10
+		}
+	];
 
-	$: ({ latest_episode, all_episodes } = data);
+	let name = '';
+	let error = '';
+
+	const addPlayer = () => {
+		let new_player = {
+			name: name,
+			goals: 0
+		};
+
+		if (name !== '') {
+			players = [...players, new_player];
+			error = '';
+		} else {
+			error = 'Name cannot be empty';
+		}
+		name = '';
+	};
+
+	const addGoal = (index) => {
+		players[index].goals += 1;
+	};
+	const removeGoal = (index) => {
+		players[index].goals -= 1;
+	};
+	const deletePlayer = (index) => {
+		let deleteItem = players[index];
+		players = players.filter((item) => item !== deleteItem);
+	};
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<h2>{latest_episode.title}</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<input type="text" placeholder="Add new player" bind:value={name} />
+<button on:click={addPlayer}>Add</button>
 <br />
-<nav>
-	{#each all_episodes as episode}
-		<a href="/episodes/{episode.slug}">{episode.title}</a><br />
-	{/each}<br />
-</nav>
+
+<ul>
+	{#each players as player, index}
+		<li>
+			<span>{player.name} - </span>
+			<span>{player.goals} goals</span>
+			<span><button on:click={() => addGoal(index)}>+</button></span>
+			<span><button on:click={() => removeGoal(index)}>-</button></span>
+			<span><button on:click={() => deletePlayer(index)}>Delete</button></span>
+		</li>
+	{:else}
+		<p>There are no players</p>
+	{/each}
+	<p class="error">{error}</p>
+</ul>
+
 <a href="/about">About Us</a>
 
-<Test />
+<style>
+	.error {
+		color: red;
+	}
+</style>
